2762f099b24edc0b1ff847b315e290cb7f792ae9
rakibullahsazib <rakibullahsazib@yahoo.com>
2022-02-14 18:38:26 +0600

    basic tailwind and css changes on init

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,22 @@ module.exports = {
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
+    colors: {
+      white: '#fffbff',
+      black: '#000000',
+      highlight: '#EDAE49',
+      'grey-5': '#C2C7CE',
+      'grey-7': '#7A8089',
+      'grey-9': '#202124',
+      yellow: '#f5b759',
+      green: '#99F997',
+      'green-dark': '#0A7908',
+      pink: '#F6A4C5',
+      blue: '#7AF2F4',
+      violet: '#B7B6FF',
+      red: '#F37979',
+      'red-dark': '#F37979'
+    },
     extend: {},
   },
   plugins: [],

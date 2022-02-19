19ba4f1d8930fa71686c0ad9d29fdd2be4312ce9
rakibullahsazib <rakibullahsazib@yahoo.com>
2022-02-19 12:27:55 +0600

    add current nav vue transition

--- src/components/page-sections/SidebarMenu.vue
+++ src/components/page-sections/SidebarMenu.vue
@@ -5,14 +5,19 @@
     <ul class="pt-2 mb-4 bg-grey-9 z-10 border-t border-grey-7">
       <router-link :to="{name: nav.name}" v-for="nav in navs" :key="nav.name" class="relative mt-3 mx-auto w-10 h-10 flex justify-center items-center transition-all duration-700 rounded-full" :class="{'bg-white bg-opaci': currentNav === nav.name}">
           <img :src="`/assets/icons/${nav.icon}`" :alt="nav.name" :title="nav.name" class="flex-shrink-0 w-5 h-5">
-          <img v-show="currentNav === nav.name" class="absolute left-full my-2 w-4 h-4" src="/assets/icons/triangle.svg" alt="Logo">
+          <Transition name="fade">
+            <img v-show="currentNav === nav.name" class="absolute left-full my-2 w-4 h-4" src="/assets/icons/triangle.svg" alt="Logo">
+          </Transition>
       </router-link>
     </ul>
   </div>
 </template>
 
 <script setup lang="ts">
-const currentNav = 'Board View'
+import { computed } from 'vue';
+import { useRoute } from 'vue-router';
+
+const currentNav = computed(() => useRoute().name)
 const navs = [
   {
     name: 'Board View',

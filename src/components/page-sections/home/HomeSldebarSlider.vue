2ebde5ad8908cfefe22ff8fefcc380f8e4aee97e
rakibullahsazib <rakibullahsazib@yahoo.com>
2022-03-04 20:15:51 +0600

    add workspace store

--- src/components/page-sections/home/HomeSldebarSlider.vue
+++ src/components/page-sections/home/HomeSldebarSlider.vue
@@ -7,10 +7,22 @@
         @click="isCreateWorkspaceModalShown = true"
         title="Workspaces"
       />
+      <div v-if="!workspaces?.length" class="mt-2 text-grey-7 text-xs">
+        <p>
+          No workspace created yet.
+        </p>
+        <button
+          @click="isCreateWorkspaceModalShown = true"
+          class="mt-0.5 text-grey-9 underline focus:outline-none"
+        >
+          Add a workspace
+        </button>
+      </div>
       <SingleSelectDropdown
+        v-else
         @click.stop="toggleCurrentDropdown('slider-workspaces')"
         :options="workspaces"
-        :selectedOptionId="selectedWorkspaceId"
+        :selectedOptionId="currentWorkspace?.id"
         placeholder="Select Workspace"
         :isDropdownShown="currentDropdown === 'slider-workspaces'"
         class="mt-4 text-sm"
@@ -22,25 +34,19 @@
 <script setup lang="ts">
 import { computed, ref } from 'vue';
 import { useMainStore } from '../../../store/root'
+import { useWorkspaceStore } from '../../../store/workspace'
 import SidebarSliderLayout from '../../../layouts/SidebarSliderLayout.vue';
 import HeaderAddButton from '../../buttons/HeaderAddButton.vue';
 import SingleSelectDropdown from '../../inputs/dropdowns/SingleSelectDropdown.vue';
 
 const mainStore = useMainStore()
+const workspaceStore = useWorkspaceStore()
+
+const workspaces = computed(() => workspaceStore.workspaces)
+const currentWorkspace = computed(() => workspaceStore.currentWorkspace)
+console.log(workspaces.value, currentWorkspace.value)
 const isCreateWorkspaceModalShown = ref(false)
-const workspaces = computed(() => [
-  {
-    id: 1,
-    name: 'Workspace 1',
-    icon: 'workspace.svg'
-  },
-  {
-    id: 2,
-    name: 'Workspace 2',
-    icon: 'workspace.svg'
-  }
-])
-const selectedWorkspaceId = computed(() => 1)
+
 let currentDropdown = computed(() => mainStore.currentDropdown)
 const toggleCurrentDropdown = mainStore.toggleCurrentDropdown
 </script>

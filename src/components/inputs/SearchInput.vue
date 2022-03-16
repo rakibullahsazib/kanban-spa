4f801d4d501ba140867a6c397d618fe502387e90
rakibullahsazib <rakibullahsazib@yahoo.com>
2022-03-16 12:14:16 +0600

    add search input component

---
+++ src/components/inputs/SearchInput.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="flex items-center space-x-2">
+    <input
+      @input="onInputChange"
+      type="text"
+      :id="id"
+      :placeholder="placeholder"
+      class=" flex-grow w-full focus:outline-none"
+    >
+    <label :for="id" class="block flex-shrink-0">
+      <img class="w-5 h-5" src="assets/icons/search.svg" alt="">
+    </label>
+  </div>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+  id: string,
+  placeholder: string
+}>()
+
+const emit = defineEmits(['inputChange'])
+
+const onInputChange = (event: Event) => {
+  const target = event.target as HTMLInputElement
+  emit('inputChange', target.value)
+}
+</script>
+
+<style scoped>
+
+</style>

7b28d37e9e98ddb4d6f8e6ac6152b0ecc668f995
rakibullahsazib <rakibullahsazib@yahoo.com>
2022-03-29 12:29:24 +0600

    Button With Icon added

---
+++ src/components/buttons/ButtonWithIcon.vue
@@ -0,0 +1,31 @@
+<template>
+  <button
+    :disabled="disabled"
+    class="flex-center text-grey-9 font-semibold rounded-full text-sm py-2 px-4 focus:outline-none transition-colors duration-300"
+    :class="{
+      'bg-highlight' : !type,
+      'bg-red' : type === 'danger',
+    }"
+  >
+    <img v-if="icon" :src="`/assets/icons/${icon}`" alt="" class="absolute left-4 w-4 h-4">
+    <p class="">
+      {{ title }}
+    </p>
+  </button>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+  title?: string,
+  icon?: string,
+  disabled?: boolean,
+  type?: 'primary' | 'danger'
+}>()
+</script>
+
+<style scoped>
+button:disabled{
+  background-color: #aaa;
+  opacity: 0.5;
+}
+</style>

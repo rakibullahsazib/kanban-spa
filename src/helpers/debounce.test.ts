7a7a69c023a8e5ddb7ea85b9df64a06bc2788378
rakibullahsazib <rakibullahsazib@yahoo.com>
2022-03-31 15:14:42 +0600

    add debounce helper function test

--- src/helpers/debounce.test.ts
+++ src/helpers/debounce.test.ts
@@ -1,4 +1,4 @@
-import { describe } from "vitest";
+import { describe, vi } from "vitest";
 import { debounce } from "./debounce";
 
 describe('debounce', () => {
@@ -7,16 +7,24 @@ describe('debounce', () => {
     const increase = debounce(() => {
       test++
     }, 50)
+    vi.useFakeTimers()
+    increase()
+    vi.advanceTimersByTime(40)
     expect(test).toBe(5)
   })
-  test('function is executed after specified time', () => {
+  test('function is executed after specified time', async () => {
     let test = 5
     const increase = debounce(() => {
+      console.log('increase test to', test + 1)
       test++
     }, 500)
+    vi.useFakeTimers()
+    increase()
     expect(test).toBe(5)
-    setTimeout(() => {
-      expect(test).toBe(6)
-    }, 501)
+    vi.advanceTimersByTime(100)
+    increase() // this will debounce the first increase call
+    expect(test).toBe(5)
+    vi.advanceTimersByTime(501)
+    expect(test).toBe(6)
   })
 })

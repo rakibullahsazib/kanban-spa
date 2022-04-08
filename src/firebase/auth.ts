51e5c63a643d7927ba7cccf469357e804950fa7b
rakibullahsazib <rakibullahsazib@yahoo.com>
2022-04-08 21:39:49 +0600

    refactor code

--- src/firebase/auth.ts
+++ src/firebase/auth.ts
@@ -1,5 +1,21 @@
 import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, onAuthStateChanged } from "firebase/auth";
+import { useRootStore } from "../store/rootStore";
 import { useUserStore } from "../store/userStore";
-const userStore = useUserStore()
 
 const auth = getAuth();
+export const listenFirebaseAuthState = () => onAuthStateChanged(auth, (user) => {
+  const userStore = useUserStore()
+  const rootStore = useRootStore()
+  if (user) {
+    // User is signed in, see docs for a list of available properties
+    // https://firebase.google.com/docs/reference/js/firebase.User
+    userStore.uid = user.uid
+    if (user.email) {
+      userStore.email = user.email;
+    }
+    userStore.loggedIn = true
+    console.log('loggedIn', userStore.email, userStore.uid)
+  } else {
+    rootStore.resetStores()
+  }
+});
